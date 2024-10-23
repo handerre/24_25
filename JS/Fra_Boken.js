a895087597bfee02b0526737e6daba9ecf3a486c
John Doe <johndoe@example.com>
2024-10-23 15:19:58 +0200

    JS2

--- JS/Fra_Boken.js
+++ JS/Fra_Boken.js
@@ -5,7 +5,7 @@ alert("Jeg heter Bent")
 //En boks som dukker opp hvor du kan skrive ditt navn
 prompt("Hva heter du?")
 
-//En funksjon som tar i mot tekstinputt og skriver det ut på nettsiden sammen med hallo og har du det bra!
+//En funksjon som tar i mot tekstinputt og skriver det ut på nettsiden sammen med; hallo og har du det bra!
 function myFunction() {
   let text;
   let person = prompt("Skriv inn ditt navn:", "");
