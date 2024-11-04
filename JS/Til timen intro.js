e46dd1caef938c6d3d7e387422f9ffdaec249ec8
Handerre <bent.oystein.halden@vlfk.no>
2024-11-04 11:47:33 +0100

    tt

---
+++ JS/Til timen intro.js
@@ -0,0 +1,15 @@
+// To variabler med ulike datatyper
+let alder = 16;
+let tekst = "Du er tenåring";
+let tall1 = 5;
+let tall2 = 6;
+
+console.log (alder + " "+ tekst);
+console.log ((tall1 + tall2) /2);
+
+// Denne linjen setter inn variablene i HTML-elementet med id "demo2"
+document.getElementById("demo2").innerHTML = alder + tekst;
+
+ // Viser en prompt-dialogboks til brukeren for å be om navn.
+  //let person = prompt("Skriv inn ditt navn:", "");
+  
