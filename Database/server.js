4fd9347fc02df4342f0ae444575ba3801e9f995a
Handerre <bent.oystein.halden@vlfk.no>
2025-01-28 12:59:49 +0100

    28.1

--- Database/server.js
+++ Database/server.js
@@ -1,23 +1,52 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
 // Sett opp EJS som template-motor
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-// Koble til SQLite-databasen
-const db = new sqlite3.Database('database.db');
+// Koble til SQLite-databaser
+const dbUsers = new sqlite3.Database('./database.db', (err) => {
+    if (err) console.error(err.message);
+    console.log('Koblet til user-databasen.');
+});
+const dbBiler = new sqlite3.Database('./Biler.db', (err) => {
+    if (err) console.error(err.message);
+    console.log('Koblet til Biler-databasen.');
+});
 
-// Hovedrute for å vise brukere
+
+
+// Hovedrute
 app.get('/', (req, res) => {
-    db.all('SELECT * FROM users', [], (err, rows) => {
+    res.render('index', { title: 'Velkommen til mine databaser' });
+});
+// Rute for å vise brukere
+app.get('/users', (req, res) => {
+    const query = `SELECT * FROM users`;
+    dbUsers.all(query, [], (err, rows) => {
         if (err) {
             console.error(err.message);
-            res.status(500).send("Database error!");
+            res.status(500).send("Database feil/error!");
         } else {
-            res.render('index', { users: rows });
+            res.render('users', { title:'Brukere', users: rows });
+        }
+    });
+});
+
+// Rute for å vise bilinformasjon
+app.get('/biler', (req, res) => {
+    const query = `SELECT * FROM Bil`;
+    dbBiler.all(query, [], (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).send('Feil ved henting av data.');
+        } else {
+            res.render('biler', { title: 'Biloversikt', biler: rows });
         }
     });
 });
--- Database/server2.js
+++ Database/server2.js
@@ -1,7 +1,5 @@
-// Importer nødvendige moduler
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
-const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
@@ -10,66 +8,16 @@ const PORT = 3000;
 app.set('view engine', 'ejs');
 
 // Koble til SQLite-databasen
-const sqliteDb = new sqlite3.Database('Biler.db');
+const db = new sqlite3.Database('Biler.db');
 
-
-// Rute for å vise brukere fra SQLite-databasen
-app.get('/sqlite', (req, res) => {
-    sqliteDb.all('SELECT * FROM users', [], (err, rows) => {
+// Hovedrute for å vise brukere
+app.get('/', (req, res) => {
+    db.all('SELECT * FROM Bil', [], (err, rows) => {
         if (err) {
             console.error(err.message);
-            res.status(500).send("SQLite Database error!");
+            res.status(500).send("Database error!");
         } else {
-            res.render('index', { users: rows }); // Render data fra SQLite
-        }
-    });
-});
-
-// Rute for å vise brukere og biler fra MySQL-databasen
-app.get('/mysql', (req, res) => {
-    const query = `
-        SELECT eier.*, poststed.Poststed, bil.* 
-        FROM Eier eier
-        LEFT JOIN Poststed poststed ON eier.Postnummer = poststed.Postnummer
-        LEFT JOIN Eier_Bil eier_bil ON eier.Personnummer = eier_bil.Personnummer
-        LEFT JOIN Bil bil ON eier_bil.Registreringsnummer = bil.Registreringsnummer;
-    `;
-
-    mysqlDb.query(query, (err, results) => {
-        if (err) {
-            console.error(err.message);
-            res.status(500).send("MySQL Database error!");
-        } else {
-            const users = results.reduce((acc, row) => {
-                const user = acc[row.Personnummer] || {
-                    fornavn: row.Fornavn,
-                    etternavn: row.Etternavn,
-                    mobil: row.Mobil,
-                    gateadresse: row.Gateadresse,
-                    gatenummer: row.Gatenummer,
-                    email: row.E_post,
-                    postnummer: row.Postnummer,
-                    poststed: row.Poststed,
-                    biler: []
-                };
-
-                if (row.Registreringsnummer) {
-                    user.biler.push({
-                        registreringsnummer: row.Registreringsnummer,
-                        merke: row.Merke,
-                        modell: row.Modell,
-                        km: row.Km,
-                        drivstofftype: row.Drivstofftype,
-                        farge: row.Farge,
-                        registreringsdato: row.Registreringsdato
-                    });
-                }
-
-                acc[row.Personnummer] = user;
-                return acc;
-            }, {});
-
-            res.render('index', { users: Object.values(users) }); // Render data fra MySQL
+            res.render('index', { users: rows });
         }
     });
 });
